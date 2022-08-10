c5fa8d5982635dc59ca835899d38e1ca470ce656
BGRG059 <102294756+BGRG059@users.noreply.github.com>
2022-08-10 13:06:12 +0100

    Add files via upload
    This data was used in Tableau Tutorial. I have used the same data to extract same result using SQL as shown in the graph labelled Tutorial - Kirill (https://public.tableau.com/app/profile/b.grg/viz/Tutorial-Kirill/Sheet1)

---
+++ OfficeSupplies.sql
@@ -0,0 +1,29 @@
+
+-- Link to data: OfficeSupplies @ www.artofvisualization.com/pages/tableau
+
+SELECT	*
+FROM	[dbo].['P1-OfficeSupplies$']
+
+-- Displays total units sold by reps in a descending order
+SELECT		rep, sum(units) TotUnitsSold
+FROM		[dbo].['P1-OfficeSupplies$']
+GROUP BY	rep
+ORDER BY	TotUnitsSold desc
+
+-- Displays total units sold by reps. Here reps are grouped according to their region.
+SELECT		region, rep, sum(units) TotUnitsSold
+FROM		[dbo].['P1-OfficeSupplies$']
+GROUP BY	rep, region
+ORDER BY	region, TotUnitsSold desc
+
+-- Calculating total revenue
+SELECT		sum(Round(Units*"UNIT PRICE",0)) TotalSales
+FROM		[dbo].['P1-OfficeSupplies$']
+GROUP BY	rep, region
+
+-- Displays total revenue per  reps with highest on top. Here reps are grouped according to their region.
+-- Applying Concat('$ ', Sum(Round(Units*"UNIT PRICE",0))) to display $ in front of tot sales figures doesn't allow tot sales column to be displayed in desc/asc order.
+SELECT		region, rep, Sum(Round(Units*"UNIT PRICE",0)) AS TotalSales
+FROM		[dbo].['P1-OfficeSupplies$']
+GROUP BY	rep, region
+ORDER BY	region, TotalSales desc
